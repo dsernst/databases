1330e86bd05df324ee9a10a51dffe729aa8cd2ab
dsernst <github@dsernst.com>
2015-01-09 17:35:42 -0800

    Database Schema ftw.

--- server/schema.sql
+++ server/schema.sql
@@ -3,13 +3,30 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int(5) NOT NULL auto_increment,
+  date TIMESTAMP NOT NULL,
+  text varchar(140) NOT NULL,
+  roomid int(3) NOT NULL,
+  userid int(5) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE rooms (
+  id int(3) NOT NULL auto_increment,
+  name varchar(25) NOT NULL,
+  PRIMARY KEY (id)
+);
 
 
+CREATE TABLE users (
+  id int(5) NOT NULL auto_increment,
+  username varchar(15) NOT NULL,
+  password varchar(25),
+  email varchar(30),
+  PRIMARY KEY (id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
